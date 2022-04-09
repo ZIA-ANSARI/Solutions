858fd9a8be6513b4f2fa8f138d7aab8a4ce2c34e
Zia <ansarizia3951@gmail.com>
2022-04-09 17:23:15 +0530

    "try1"

---
+++ src/TargetSum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class TargetSum {
+
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+          System.out.println("Hwyya");
+          int[] question = new int[]{3,2,4, 11};
+          
+    }
+}
