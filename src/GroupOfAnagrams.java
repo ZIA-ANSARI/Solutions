e8cfb0d74baa65cf085a33ed358f0f6442aa20c5
Zia <ansarizia3951@gmail.com>
2022-04-27 09:47:44 +0530

    "Group of anagrams: java issues in hashmapping"

---
+++ src/GroupOfAnagrams.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupOfAnagrams {
+    
+    public static void main(String[] args) {
+        String[] s = new String[]{"eat","tea","tan","ate","nat","bat"};
+        GroupOfAnagrams groupOfAnagrams = new GroupOfAnagrams();
+        System.out.println(groupOfAnagrams.groupAnagrams(s));
+        
+    }
+    public List<List<String>> groupAnagrams(String[] s){
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String, List<String>> hash = new HashMap<>();
+        for(int i = 0 ; i< s.length; i++){
+            int[] key = new int[26];
+            //ArrayList<Boolean> key = new ArrayList<>(Arrays.asList(false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false));
+            char[] temp = s[i].toCharArray();
+            for(char c : temp){
+               // System.out.println((int) c - 'a');
+                key[(int) c - 'a']++;
+            }
+            
+            System.out.println(Arrays.toString(key));
+            if(!hash.containsKey(Arrays.toString(key))){
+                ArrayList<String> strs = new ArrayList<>();
+                strs.add(s[i]);
+                hash.put(Arrays.toString(key), strs);
+                System.out.println("in if");
+            }else{
+                List<String> strs = hash.get(Arrays.toString(key));
+                strs.add(s[i]);
+                hash.put(Arrays.toString(key), strs);
+                System.out.println("else");
+            }
+        }
+        for(List<String> str: hash.values()){
+            res.add(str);
+        }
+        return res;
+    }
+}
